0b93165c978579e606cc3ea42081b743357942e4
Yoyo276 <117311038+Yoyo276@users.noreply.github.com>
2026-04-13 22:00:44 +0200

    Setup Springboot

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("SISTEMA: HackHub è online su http://localhost:8080");
+    }
+}
