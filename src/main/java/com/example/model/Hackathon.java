f87ecce6d9f27cec4854d06456aaaf40076d1e39
NicolaCase <nicola.casenove@studenti.unicam.it>
2026-04-27 22:35:13 +0200

    Update Hackathon.java

--- src/main/java/com/example/model/Hackathon.java
+++ src/main/java/com/example/model/Hackathon.java
@@ -19,19 +19,43 @@ public class Hackathon {
     private String nome;
     private String luogo;
     private Double premioInDenaro;
+    private String nomeStato = "IN ISCRIZIONE";
+
+    // --- NUOVI CAMPI PER IL CALENDAR E REGOLE ---
+    private LocalDate dataScadenzaIscrizioni;
+    private LocalDate dataInizio;
+    private LocalDate dataFine;
+    private Integer dimensioneMassimaTeam;
+
+    @Transient
+    private HackathonState statoAttuale;
 
     @ManyToOne
     private User organizzatore;
+
+    @ManyToOne
+    private User giudiceAssegnato;
+
+    // --- NUOVO: LISTA DI MENTORI ---
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<User> mentori = new ArrayList<>();
 
     @OneToMany(mappedBy = "hackathon", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonIgnoreProperties("hackathon")
     private List<Team> teams = new ArrayList<>();
 
     public Hackathon() {
-
-
+        this.statoAttuale = new InIscrizioneState();
     }
 
+    public HackathonState getStato() {
+        return statoAttuale;
+    }
+
+    public void setStato(HackathonState nuovoStato) {
+        this.statoAttuale = nuovoStato;
+        this.nomeStato = nuovoStato.getNomeStato();
+    }
 
     // Getter e Setter
     public Long getId() {
@@ -65,7 +89,55 @@ public class Hackathon {
     public void setPremioInDenaro(Double p) {
         this.premioInDenaro = p;
     }
-    
+
+    public String getNomeStato() {
+        return nomeStato;
+    }
+
+    public void setNomeStato(String s) {
+        this.nomeStato = s;
+    }
+
+    public LocalDate getDataScadenzaIscrizioni() {
+        return dataScadenzaIscrizioni;
+    }
+
+    public void setDataScadenzaIscrizioni(LocalDate dataScadenzaIscrizioni) {
+        this.dataScadenzaIscrizioni = dataScadenzaIscrizioni;
+    }
+
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+
+    public void setDataInizio(LocalDate dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public LocalDate getDataFine() {
+        return dataFine;
+    }
+
+    public void setDataFine(LocalDate dataFine) {
+        this.dataFine = dataFine;
+    }
+
+    public Integer getDimensioneMassimaTeam() {
+        return dimensioneMassimaTeam;
+    }
+
+    public void setDimensioneMassimaTeam(Integer dimensioneMassimaTeam) {
+        this.dimensioneMassimaTeam = dimensioneMassimaTeam;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+
     public User getOrganizzatore() {
         return organizzatore;
     }
@@ -74,5 +146,19 @@ public class Hackathon {
         this.organizzatore = o;
     }
 
+    public User getGiudiceAssegnato() {
+        return giudiceAssegnato;
+    }
 
+    public void setGiudiceAssegnato(User g) {
+        this.giudiceAssegnato = g;
+    }
+
+    public List<User> getMentori() {
+        return mentori;
+    }
+
+    public void setMentori(List<User> mentori) {
+        this.mentori = mentori;
+    }
 }
