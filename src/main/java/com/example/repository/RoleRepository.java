834f2ac3a06a6287ea4260bb50ece6dd13be6c1f
NicolaCase <nicola.casenove@studenti.unicam.it>
2026-05-03 20:00:34 +0200

    Update RoleRepository.java

--- src/main/java/com/example/repository/RoleRepository.java
+++ src/main/java/com/example/repository/RoleRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    // Questo metodo ci serve per cercare "ROLE_MEMBRO_TEAM" nel DB
     Optional<Role> findByName(String name);
 }
